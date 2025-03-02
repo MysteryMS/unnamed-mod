376219e1a9568680530d765cf4f462031aa33bf3
Matheus Sanches <methidezeeni@gmail.com>
2025-03-02 18:04:55 -0300

    chore: remove println

--- src/client/kotlin/com/example/networking/extensions/RefineryScreenUpdatePayloadExecutor.kt
+++ src/client/kotlin/com/example/networking/extensions/RefineryScreenUpdatePayloadExecutor.kt
@@ -5,7 +5,6 @@ import com.example.screen.RefineryScreenHandler
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 
 fun RefineryScreenUpdatePayload.execute(ctx:  ClientPlayNetworking.Context) {
-    println("RefineryScreenUpdatePayload: $this")
     ctx.client().execute {
         val currentScreenHandler = ctx.player().currentScreenHandler
         if (currentScreenHandler is RefineryScreenHandler && currentScreenHandler.entity.pos == this.blockPos) {
